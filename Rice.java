75da2eeb344ae2e089e9cc58a8d41fb2c1b2cf07
Varsha Gnanasekar <155064325+VarshaGnanasekar@users.noreply.github.com>
2024-11-04 19:14:14 +0530

    Add files via upload

---
+++ Rice.java
@@ -0,0 +1,15 @@
+#import java.util.Scanner
+class Rice
+{
+	public Static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int t_k,f_k;
+		System.out.println("Enter the no of 2 kg rice bags:");
+		t_k=sc.nextInt();
+		System.out.println("Enter the no of 5 kg rice bags:");
+		f_k=sc.nextInt();
+		int avail_total=t_k+f_v;
+		System.out.println("TOTAL AMOUNT OF RICE AVAILABLE:"+avail_total);
+	}
+}
